54b86f040e25d99362b6047d62d016ef48bc801b
annnacaesar <Anechka22334@gmail.com>
2022-12-08 21:43:09 +0200

    p

--- src/services/MailService.js
+++ src/services/MailService.js
@@ -8,7 +8,7 @@ class MailService {
       secure: false,
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        pass: process.env.SMTP_P
       }
     });
   }
