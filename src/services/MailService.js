c4534b0be6fe842dc736288b567bce21e94934c1
annnacaesar <Anechka22334@gmail.com>
2022-12-08 22:17:29 +0200

    Update MailService.js

--- src/services/MailService.js
+++ src/services/MailService.js
@@ -5,7 +5,7 @@ class MailService {
     this.trnasporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_P
